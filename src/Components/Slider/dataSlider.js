1def19d802201d99e71ad8de9c12d29904d253e9
Agourovanidis <Agourovanidis@mzt.gr>
2021-08-08 19:57:49 +0300

    1st

---
+++ src/Components/Slider/dataSlider.js
@@ -0,0 +1,50 @@
+// import { v4 as uuidv4 } from "uuid";
+
+const dataSlider = [
+    'https://cdn.mouzenidis-travel.ru/static/userfiles/hotels/original/111/111_julia-hotel_188235.jpeg?preset=b',
+    'https://cdn.mouzenidis-travel.ru/static/userfiles/hotels/original/111/111_julia-hotel_188255.jpeg?preset=b',
+    'https://cdn.mouzenidis-travel.ru/static/userfiles/hotels/original/111/111_julia-hotel_188236.jpeg?preset=b',
+    'https://cdn.mouzenidis-travel.ru/static/userfiles/hotels/original/111/111_julia-hotel_188233.jpeg?preset=b',
+    'https://cdn.mouzenidis-travel.ru/static/userfiles/hotels/original/111/111_julia-hotel_188254.jpeg?preset=b',
+    'https://cdn.mouzenidis-travel.ru/static/userfiles/hotels/original/111/111_julia-hotel_105748.jpeg?preset=b',
+    'https://cdn.mouzenidis-travel.ru/static/userfiles/hotels/pictureshd/111/33621.jpeg?preset=b',
+    'https://cdn.mouzenidis-travel.ru/static/userfiles/hotels/original/111/111_julia-hotel_188253.jpeg?preset=b',
+    'https://cdn.mouzenidis-travel.ru/static/userfiles/hotels/original/111/111_julia-hotel_188241.jpeg?preset=b',
+    'https://cdn.mouzenidis-travel.ru/static/userfiles/hotels/original/111/111_julia-hotel_188251.jpeg?preset=b',
+    'https://cdn.mouzenidis-travel.ru/static/userfiles/hotels/original/111/111_julia-hotel_188234.jpeg?preset=b',
+    'https://cdn.mouzenidis-travel.ru/static/userfiles/hotels/original/111/111_julia-hotel_188222.jpeg?preset=b',
+    'https://cdn.mouzenidis-travel.ru/static/userfiles/hotels/original/111/111_julia-hotel_188240.jpeg?preset=b',
+    'https://cdn.mouzenidis-travel.ru/static/userfiles/hotels/original/111/111_julia-hotel_188248.jpeg?preset=b',
+    'https://cdn.mouzenidis-travel.ru/static/userfiles/hotels/original/111/111_julia-hotel_188246.jpeg?preset=b',
+    'https://cdn.mouzenidis-travel.ru/static/userfiles/hotels/pictureshd/111/33627.jpeg?preset=b',
+    'https://cdn.mouzenidis-travel.ru/static/userfiles/hotels/original/111/111_julia-hotel_105745.jpeg?preset=b',
+    'https://cdn.mouzenidis-travel.ru/static/userfiles/hotels/pictureshd/111/33633.jpeg?preset=b'
+
+//   {
+//     id: uuidv4(),
+//     title: "Lorem ipsum",
+//     subTitle: "Lorem"
+//   },
+//   {
+//     id: uuidv4(),
+//     title: "Lorem ipsum",
+//     subTitle: "Lorem"
+//   },
+//   {
+//     id: uuidv4(),
+//     title: "Lorem ipsum",
+//     subTitle: "Lorem"
+//   },
+//   {
+//     id: uuidv4(),
+//     title: "Lorem ipsum",
+//     subTitle: "Lorem"
+//   },
+//   {
+//     id: uuidv4(),
+//     title: "Lorem ipsum",
+//     subTitle: "Lorem"
+//   },
+];
+
+export default dataSlider;
